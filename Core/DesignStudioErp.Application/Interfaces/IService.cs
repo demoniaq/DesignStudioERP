512cb16d2ac90c9a9e9a4cfa857d55cffd940a09
Dmitry Ocherednuk <idemonout@icloud.com>
2021-11-27 14:10:47 +0500

    Refactor

--- Core/DesignStudioErp.Application/Interfaces/IService.cs
+++ Core/DesignStudioErp.Application/Interfaces/IService.cs
@@ -2,7 +2,7 @@ using DesignStudioErp.Domain;
 
 namespace DesignStudioErp.Application.Interfaces;
 
-public interface IService<TEntity>
+public interface IService<TEntity> where TEntity : BaseModel
 {
     Task<IEnumerable<TEntity>> GetAllAsync();
 
